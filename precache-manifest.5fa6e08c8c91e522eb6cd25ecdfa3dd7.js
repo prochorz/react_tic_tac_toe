bc7f58f766340dc5c3767d0fc5bface668143f29
Prochor Simon <prokhor.zimon@agilie.com>
2020-01-10 10:08:45 +0200

    Updates

---
+++ precache-manifest.5fa6e08c8c91e522eb6cd25ecdfa3dd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401b6d667a93571f6c6d1efdd5881474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a89ed946777189fc75e9",
+    "url": "/static/css/main.8bf38417.chunk.css"
+  },
+  {
+    "revision": "eea4ce8728721d1fdaec",
+    "url": "/static/js/2.35d789b9.chunk.js"
+  },
+  {
+    "revision": "a89ed946777189fc75e9",
+    "url": "/static/js/main.9014aac5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
